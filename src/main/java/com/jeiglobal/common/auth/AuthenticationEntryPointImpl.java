65a3c768d511613eea0711e6c057683bef7ff1a6
qwio2000 <qwio2000@naver.com>
2015-09-09 15:44:45 +0900

    test

--- src/main/java/com/jeiglobal/common/auth/AuthenticationEntryPointImpl.java
+++ src/main/java/com/jeiglobal/common/auth/AuthenticationEntryPointImpl.java
@@ -5,11 +5,13 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import org.slf4j.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.security.core.*;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.util.*;
 import org.springframework.stereotype.*;
+
 /**
  * 
  * 클래스명 : AuthenticationEntryPointImpl.java
@@ -25,6 +27,8 @@ import org.springframework.stereotype.*;
 @Component
 public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
 	
+	private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationEntryPointImpl.class);
+	
 	private String loginFormPath;
 	
 	@Value("${serverurl.globalbms}")
@@ -38,14 +42,14 @@ public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
 			setLoginFormPath("/login");
 		}
 		String redirectUrl = UrlUtils.buildRequestUrl(request);
-		System.out.println("redirectUrl : "+redirectUrl);
+		LOGGER.debug("redirectUrl : {}", redirectUrl);
 		String encodedUrl = response.encodeRedirectURL(redirectUrl);
-		System.out.println("encodedUrl : "+encodedUrl);
+		LOGGER.debug("encodedUrl : {}", encodedUrl);
 		if("/".equals(encodedUrl)){
-			System.out.println("URL : "+globalbmsUrl+loginFormPath);
+			LOGGER.debug("URL : {}", globalbmsUrl+loginFormPath);
 			response.sendRedirect(globalbmsUrl+loginFormPath);
 		}else{
-			System.out.println("URL : "+globalbmsUrl+loginFormPath+"?returl="+encodedUrl);
+			LOGGER.debug("URL : {}", globalbmsUrl+loginFormPath+"?returl="+encodedUrl);
 			response.sendRedirect(globalbmsUrl+loginFormPath+"?returl="+encodedUrl);
 		}
 	}
